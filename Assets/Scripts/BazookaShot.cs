4df01ebe8a07df3410ff69cdc237d473159529d4
Evan Closson <evan.closson@gmail.com>
2016-10-11 14:14:32 -0600

    Removed and re-added one sound.

---
+++ Assets/Scripts/BazookaShot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawn : MonoBehaviour {
+
+	/*Create an offset to where the rocket launcher is in reference
+	* to the player*/
+	public Vector3 rocketOffset = new Vector3(2.45f, 3.5f, 0);
+	public GameObject bulletPre;
+	public GameObject fireAnim;
+	int bulletLayer;
+
+	public AudioClip shot;
+
+	/*Delay in which the rocket is fired from the launcher. Adjust for sound.*/
+	public float delay = 0.25f;
+	public float coolDown = 0;
+
+	// Use this for initialization
+	void Start () {
+		bulletLayer = gameObject.layer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		coolDown -= Time.deltaTime;
+		if (Input.GetButton ("Fire1") && coolDown <= 0) {
+			/*Begin to shoot the rocket in the trajectory! Yeah!*/
+			coolDown = delay;
+
+			Vector3 offset = transform.rotation * rocketOffset;
+			GameObject bulletShot = (GameObject)Instantiate (bulletPre, transform.position + offset, 
+				transform.rotation * Quaternion.Euler (0, 0, 90));
+			bulletShot.layer = bulletLayer;
+			GameObject smoke = (GameObject)Instantiate (fireAnim, transform.position +
+				offset, transform.rotation * Quaternion.Euler (0, 0, 90));
+
+			/*Play the audio now.*/
+			AudioSource shotsFired = GetComponent<AudioSource> ();
+			shotsFired.PlayOneShot (shot);
+
+			/*Now remove the smoke from the bazooka.*/
+			Destroy (smoke, 0.75f);
+			Destroy (bulletShot, 10);
+			/*Reset cool down.*/
+			coolDown = 1;
+		}
+	}
+}
---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+	public GameObject toBeDestroyed;
+	public AudioClip explosion;
+	public AudioClip deadArachnid;
+
+	/*Game controller to reinitialize spiders.*/
+	private SpiderSpawns gControl;
+
+	// Use this for initialization
+	void Start () {
+		GameObject gControlObj = GameObject.FindWithTag ("SpiderSpawns");
+		if (gControlObj != null)
+			gControl = gControlObj.GetComponent<SpiderSpawns> ();
+		if (gControl == null)
+			Debug.Log ("Cannot find \"SpiderSpawn\" script");
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D (Collision2D collider) {
+		AudioSource aud = GetComponent<AudioSource> ();
+		/*If we hit a wall or spider, we can enter this.*/
+		if (collider.gameObject.tag == "spider" || collider.gameObject.tag == "wall") {
+			/*Now check if it is either a wall or a spider.*/
+			if (collider.gameObject.tag == "spider"){
+				Destroy(collider.gameObject.tag == "spider");
+				aud.PlayOneShot (deadArachnid);
+				scoreManager.score += 5;
+			}
+			aud.PlayOneShot (explosion);
+			/*Remove the rocket from existence.*/
+			gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+			Destroy (GetComponent<BoxCollider2D> ());
+			Destroy (gameObject, 5f);
+			GameObject explode = (GameObject)Instantiate(toBeDestroyed, transform.position,
+				transform.rotation * Quaternion.Euler(0,0,90));
+			/*Remove the smoke.*/
+			Destroy (explode, 0.9f);
+		}
+	}
+}
